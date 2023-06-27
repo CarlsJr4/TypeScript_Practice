dca8f81f9a1d44c20445dcfc3fe56c21fea4e4dd
Carl Dungca <carl.dungca@yahoo.com>
2023-06-26 22:04:21 -0700

    Advanced types exercises

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-let age: number = 20;
-if (age < 50) age += 10;
+type User = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+
+type Bird = {
+  fly: true;
+};
+
+type Fish = {
+  swim: true;
+};
+
+let pet: Bird | Fish;
+
+type Day = 'Monday' | 'Tuesday' | 'Wednesday';
+
+// let user = getUser();
+// console.log(user && user.address ? user.address.street : undefined);
+// console.log(user?.address?.street);
+
+// let x = foo !== null && foo !== undefined ? foo : bar();
+// let x = foo ?? bar();
+
+// let value: unknown = 'a'
+// console.log(value.toUpperCase());
+// The problem is that the unknown type hasn't been narrowed yet
