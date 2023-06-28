a03a578f426cc7e5f4709a2122752f379359e3eb
Carl Dungca <carl.dungca@yahoo.com>
2023-06-28 00:20:45 -0700

    Generics exercises

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,14 @@
 "use strict";
-class Logger {
-    constructor(file) {
-        this.file = file;
-    }
-    log(message) {
-        console.log(message);
+function echo(arg) {
+    return arg;
+}
+function printName(obj) {
+    console.log(obj.name);
+}
+class Entity {
+    constructor(ID) {
+        this.ID = ID;
     }
 }
-class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    get fullName() {
-        return this.firstName + ' ' + this.lastName;
-    }
-}
-class Employee extends Person {
-    constructor(salary, firstName, lastName) {
-        super(firstName, lastName);
-        this.salary = salary;
-    }
-}
+printName({ name: 'Carl' });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,20 @@
-// OOP Exercises
-class Logger {
-  constructor(public file: string) {}
-
-  log(message: string) {
-    console.log(message);
-  }
+function echo<T>(arg: T) {
+  return arg;
 }
 
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
-
-  get fullName() {
-    return this.firstName + ' ' + this.lastName;
-  }
+function printName<T extends { salary: number }>(obj: T) {
+  console.log(obj.salary);
 }
 
-class Employee extends Person {
-  constructor(public salary: number, firstName: string, lastName: string) {
-    super(firstName, lastName);
-  }
+class Entity<T> {
+  constructor(public ID: T) {}
 }
 
-// Private members are only accessible within the parent class. Cannot be inherited.
-// Protected members can be inherited
-
-interface Address {
-  street: string;
-  city: string;
-  zipCode: string;
+interface User {
+  userId: number;
+  usrename: string;
 }
 
-interface Employee2 {
-  name: string;
-  salary: number;
-  address: Address;
-}
+// keyof User will return an interable object that ccontains every key
+
+printName({ salary: 2 });
